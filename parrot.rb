5a9e9aee584b6d637d81d8c53a0edc22ca9f7985
Your Name <you@example.com>
2018-06-10 11:33:00 -0400

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
+def parrot(phrase = "Squawk!")
+  # define the method parrot and it takes the argument phrase
+  puts "#{phrase}"
+# puts {phrase} prints our variable to the console/ terminal
+
+return phrase
 # returns the phrase
+
+end
+# ends our method
